d107dc05117a7b3fa954d83ca7310d59af9b2c5a
mapengsen <ma_pengsen@163.com>
2020-09-05 10:02:21 +0800

    dtu的service

---
+++ src/main/java/edu/xpu/cs/lovexian/app/appadmin/service/IDtusAdminService.java
@@ -0,0 +1,46 @@
+package edu.xpu.cs.lovexian.app.appadmin.service;
+
+import com.baomidou.dynamic.datasource.annotation.DS;
+
+
+import edu.xpu.cs.lovexian.app.appadmin.entity.AdminDtus;
+import edu.xpu.cs.lovexian.app.appadmin.entity.AdminLawerInfo;
+import edu.xpu.cs.lovexian.common.domain.QueryRequest;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
+
+/**
+ *  Service接口
+ *
+ * @author xpu
+ * @date 2020-09-01 21:29:32
+ */
+@DS("slave")
+public interface IDtusAdminService extends IService<AdminDtus> {
+    /**
+     * 查询（分页）
+     *
+     * @param request QueryRequest
+     * @param adminDtus adminDtus
+     * @return IPage<AdminDtus>
+     */
+    IPage<AdminDtus> findDtuss(QueryRequest request, AdminDtus adminDtus);
+
+    IPage<AdminDtus> findDtusByTypeId(QueryRequest request, AdminDtus adminDtus);
+
+
+    /**
+     * 批量删除和通过id查询删除同时使用一个service
+     *
+     * @param id
+     * @return
+     */
+    boolean deleteDtus(String id);
+
+    IPage<AdminDtus> queryDtuInfo(QueryRequest request, AdminDtus adminDtus);
+
+
+
+}
