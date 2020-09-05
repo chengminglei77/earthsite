b3086025d5a38b2b22827c7392551181e819036f
mapengsen <ma_pengsen@163.com>
2020-09-05 10:01:55 +0800

    dtu的Mapper

---
+++ src/main/java/edu/xpu/cs/lovexian/app/appadmin/mapper/DtusAdminMapper.java
@@ -0,0 +1,27 @@
+package edu.xpu.cs.lovexian.app.appadmin.mapper;
+
+import com.baomidou.dynamic.datasource.annotation.DS;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import edu.xpu.cs.lovexian.app.appadmin.entity.AdminDtus;
+import edu.xpu.cs.lovexian.app.appadmin.entity.AdminLawerInfo;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.stereotype.Component;
+
+/**
+ *  Mapper
+ *
+ * @author xpu
+ * @date 2020-09-01 21:29:32
+ */
+@Component
+@DS("slave")
+public interface DtusAdminMapper extends BaseMapper<AdminDtus> {
+
+    IPage<AdminDtus> selectAll(Page page, @Param("adminDtus") AdminDtus adminDtus);
+
+    IPage<AdminDtus> queryDtuInfo(Page page, @Param("adminDtus") AdminDtus adminDtus);
+
+
+}
