fa7736361db0503e56247f1145ed1daa9361a17d
zhanganjie <1414904389@qq.com>
2021-01-04 09:40:58 +0800

    修改PerformA6

--- src/main/java/edu/xpu/cs/lovexian/app/appadmin/Kafka/PerformInstrution.java
+++ src/main/java/edu/xpu/cs/lovexian/app/appadmin/Kafka/PerformInstrution.java
@@ -95,6 +95,7 @@ public class PerformInstrution {
         String frameNum = unresovledDataMapper.checkFrameNum(InstructionUtil.getInstructionType(Message));
         if (frameNum == null){
             System.out.println("最新的数据为空，执行插入"+InstructionUtil.getFrameNum(Message));
+            unresovledDataMapper.insert(adminUnresovledData);
         }else {
             if (frameNum.equals(InstructionUtil.getFrameNum(Message))){
                 System.out.println("数据重复，舍去");
