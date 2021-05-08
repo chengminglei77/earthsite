ea5aef1b9f2344829b8cad8cfed162ceb32a6785
mapengsen <ma_pengsen@163.com>
2021-05-08 15:34:40 +0800

    influx

--- src/main/java/edu/xpu/cs/lovexian/phone/controller/influxDBController.java
+++ src/main/java/edu/xpu/cs/lovexian/phone/controller/influxDBController.java
@@ -1,7 +1,6 @@
 package edu.xpu.cs.lovexian.phone.controller;
 
 import edu.xpu.cs.lovexian.app.appadmin.utils.InfluxDbConnection;
-import edu.xpu.cs.lovexian.common.domain.EarthSiteResponse;
 import org.influxdb.dto.QueryResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,7 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -22,22 +24,40 @@ public class influxDBController {
     @Value("${spring.influx.database}")
     private String database;
 
+
     @GetMapping("/influxDBQuery")
-    public EarthSiteResponse influxDBQuery() {
-        LocalDateTime dateTime = LocalDateTime.now().minusDays(1);
-        String datetemp = dateTime.toString().substring(0, 10);
-        QueryResult results = influxDBConnection
-                .query("SELECT * FROM sensor_data where time >= '" + datetemp + "T16:00:00Z' and sensor_id = 'SDGW01DTU0201000801' order by time asc");
-        //results.getResults()是同时查询多条SQL语句的返回值，此处我们只有一条SQL，所以只取第一个结果集即可。
-        QueryResult.Result oneResult = results.getResults().get(0);
+    public Map<String, List<Object>> influxDBQuery() {
 
-        if (oneResult.getSeries() != null) {
-            List<List<Object>> valueList = oneResult.getSeries().stream().map(QueryResult.Series::getValues)
-                    .collect(Collectors.toList()).get(0);
+        String[] settingsId = {"SDGW01DTU0201000602", "SDGW01DTU0201000601"};
 
-            return EarthSiteResponse.SUCCESS().data(valueList);
+        Map<String, List<Object>> map = new HashMap<>();
+
+        for (int i = 0; i < settingsId.length; i++) {
+            LocalDateTime dateTime = LocalDateTime.now();
+            String datetemp = dateTime.toString().substring(0, 10);
+            System.out.println(settingsId[i]);
+            QueryResult results = influxDBConnection
+                    .query("SELECT * FROM sensor_data where time >= '" + datetemp + "T00:00:00Z' and sensor_id='"+settingsId[i]+"' order by time desc");
+            //results.getResults()是同时查询多条SQL语句的返回值，此处我们只有一条SQL，所以只取第一个结果集即可。
+            QueryResult.Result oneResult = results.getResults().get(0);
+            List<Object> valueList = Collections.singletonList(oneResult.getSeries().stream().map(QueryResult.Series::getValues).collect(Collectors.toList()).get(0));
+            map.put(settingsId[i], valueList);
         }
-        return EarthSiteResponse.FAIL().message("无数据");
+        return map;
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
